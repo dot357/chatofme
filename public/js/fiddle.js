43d616c15b4f2c4836b17aa463606cf5f935391d
Emre Can Eskimez <emrecaneskimez@outlook.com>
2019-04-19 19:00:03 +0300

    gifdesign

--- public/js/fiddle.js
+++ public/js/fiddle.js
@@ -1,30 +1,50 @@
 
 
-// Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+var btn3 = document.querySelector('.js-tingle-modal-3');
+btn3.addEventListener('click', function(){
+  modal.open();
+  
+}); 
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+
+
+// instanciate new modal
+var modal = new tingle.modal({
+  footer: true,
+  stickyFooter: false,
+  closeMethods: ['overlay', 'button', 'escape'],
+  closeLabel: "Close",
+  
+  onOpen: function() {
+   
+  },
+  onClose: function() {
+    modal.setContent('<h4>Gif Search</h4><form id="searchbox"><input type="text" id="search" style="color:black;" class="u-full-width"><br><button type="submit"  id="btnSearch"> Search</button></form><div class="gifs" id="searchresults"></div>');
+
+  },
+  beforeClose: function() {
+      // here's goes some logic
+      // e.g. save content before closing the modal
+      modal.setContent('');
+      return true; // close the modal
+      
   }
-}
+});
+
+// set content
+modal.setContent('<h4>Gif Search</h4><form id="searchbox"><input type="text" id="search" style="color:black;" class="u-full-width"><br><button type="submit"  id="btnSearch"> Search</button></form><div class="gifs" id="searchresults"></div>');
+
+
+
+  
+ 
+ // modal.open();
+
+
 
 
 
@@ -56,11 +76,11 @@ $searchbox.submit(function(e){
     count = data.pagination.count;
     
 
-    if(count >=5){
-      for(i=0;i<=5;i++){
+    
+      for(i=0;i<=count;i++){
       
         //buglu düzeltilmesi gereken kısım aradıktan sonra tekrar arayınca yeni query ataması lazım
-        $searchresults.append('<button id="#gif'+i+'" style="border:none;";"><img class="mobile" src='+data.data[i].images.original.url +'></button>');
+        $searchresults.append('<button id="#gif'+i+'" style="height:auto;border:none;";"><img class="mobile" src='+data.data[i].images.original.url +'></button>');
         
 
         
@@ -84,8 +104,7 @@ $searchbox.submit(function(e){
         
 
 
-        }
-
+     
       
        /* $nextpage2.submit(function(e){
 
